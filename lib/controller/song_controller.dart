be858a7d744700a1c7c1b098a40e8f545de73182
salmamamdoh441 <salma.mamdouh441@gmail.com>
2024-05-08 16:38:03 +0300

    finished home screen

--- lib/controller/song_controller.dart
+++ lib/controller/song_controller.dart
@@ -1,15 +1,16 @@
 import 'package:song/model/song_model.dart';
 
 class SongController{
+  static bool loading = true;
   static SongModel getSongInfo(){
-    // return SongModel(name: 'aw2at y donya', des: 'gamila awy', artist: 'mohamed foad');
-    return SongModel.fromjson(
-        {
-          "name" : "aw2at ya donya",
-          "des" : "gamila awy",
-          "artist" : "mohamed foad",
-        }
-
-    );
+    return SongModel(name: 'aw2at y donya', des: 'gamila awy', artist: 'mohamed foad');
+    // return SongModel.fromjson(
+    //     {
+    //       "name" : "aw2at ya donya",
+    //       "des" : "gamila awy",
+    //       "artist" : "mohamed foad",
+    //     }
+    //
+    // );
   }
 }
